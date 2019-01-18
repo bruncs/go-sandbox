d6541177d5e42b580121d86cdc41f6d40e82a877
Bruno Correia <brunocs@msn.com>
2019-01-18 00:11:28 -0200

    Add fizzbuzz to loops

--- 07_loops/main.go
+++ 07_loops/main.go
@@ -14,4 +14,18 @@ func main() {
 	for i := 1; i <= 10; i++ {
 		fmt.Printf(" %d", i)
 	}
+
+	// FizzBuzz
+	fmt.Println("\n\nFizzBuzz:")
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Printf("FizzBuzz ")
+		} else if i%5 == 0 {
+			fmt.Printf("Buzz ")
+		} else if i%3 == 0 {
+			fmt.Printf("Fizz ")
+		} else {
+			fmt.Printf("%d ", i)
+		}
+	}
 }
